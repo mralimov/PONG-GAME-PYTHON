c8a7f7ba688d8b0b23c52c7745bb6086c8146e15
mralimov <mralimov89@gmail.com>
2021-04-05 15:46:13 -0400

    import ball move and set the speed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+import time
+
 
 screen = Screen()
 screen.setup(width=900, height=600)
@@ -26,5 +28,7 @@ screen.onkey(l_paddle.down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 screen.exitonclick()
