5f43538d8165cb8595b3ed9c661b65c348a4c1b7
mralimov <mralimov89@gmail.com>
2021-04-05 15:36:53 -0400

    import paddle function to main function

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
+
 
 screen = Screen()
 screen.setup(width=900, height=600)
 screen.title("PONG-GAME")
 screen.bgcolor("black")
 screen.tracer(0)
-
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(-430, 0)
-
-
-def left_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def left_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
 
 
 screen.listen()
