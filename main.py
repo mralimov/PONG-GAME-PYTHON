13bb89af1c16fa9d863d38a3dd7b078c3d701e88
mralimov <mralimov89@gmail.com>
2021-04-06 10:38:07 -0400

    import scoreboard method

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 
@@ -25,6 +26,7 @@ screen.onkey(r_paddle.down, "Down")
 screen.onkey(l_paddle.up, 'w')
 screen.onkey(l_paddle.down, "s")
 
+scoreboard = Scoreboard()
 
 game_is_on = True
 while game_is_on:
@@ -40,11 +42,13 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
-     # Detect Right paddle misses
-    if ball.xcor() > 450 or ball.xcor() < -450:
+    # Detect Right paddle misses
+    if ball.xcor() > 450:
         ball.reset_ball()
+        scoreboard.l_point()
 
     if ball.xcor() < -450:
         ball.reset_ball()
+        scoreboard.r_point()
 
 screen.exitonclick()
