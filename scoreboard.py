c56df2cd34c4ac0ecbba7f087073e7639cb34a6f
mralimov <mralimov89@gmail.com>
2021-04-06 10:36:10 -0400

    clear scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -13,6 +13,7 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        self.clear()
         self.goto(-100, 200)
         self.write(self.l_score, align="center",
                    font=("Courier", 80, "normal"))
