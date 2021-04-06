1512be04a7223c5e9eecde347301c6121d29e8c2
mralimov <mralimov89@gmail.com>
2021-04-06 10:33:45 -0400

    setting position for scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -10,3 +10,9 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.l_score = 0
         self.r_score = 0
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center",
+                   font=("Courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(self.r_score, align="center",
+                   font=("Courier", 80, "normal"))
